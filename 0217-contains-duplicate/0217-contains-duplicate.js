8e96097848b87f345789f2c3ed6ebbf31a30b499
mohammed-42 <mohammedmustafa5156@gmail.com>
2026-06-28 17:46:36 +0530

    Time: 49 ms (10.99%), Space: 82 MB (5.36%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let seen={};
+    for(let num of nums){
+        if(seen[num]){
+            return true;
+        }
+        seen[num]=true;
+    }
+    return false;
+    
+};
