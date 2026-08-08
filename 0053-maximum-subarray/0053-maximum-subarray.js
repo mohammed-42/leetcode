6152a52d53ef1a563575dc2fb34fb7bfd746b064
mohammed-42 <mohammedmustafa5156@gmail.com>
2026-08-08 23:08:09 +0530

    Time: 1 ms (87.33%), Space: 63.8 MB (64.63%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -3,14 +3,17 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-let currentsum=nums[0];
-  let maxsum=nums[0];
+let currentSum=nums[0]
+  let maxsum=nums[0]
   for(let i=1;i<nums.length;i++){
-    let sum=Math.max(nums[i],currentsum+nums[i]);
-    currentsum=sum;
-    if(currentsum>maxsum){
-      maxsum=currentsum;
-    }
+     if(nums[i]> currentSum +nums[i]){
+      currentSum=nums[i]
+     }else{
+      currentSum=currentSum+nums[i]
+     }
+     if(currentSum>maxsum){
+      maxsum=currentSum
+     }
   }
   return maxsum;
 }
