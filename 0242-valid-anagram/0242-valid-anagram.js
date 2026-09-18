4df0d6ef2c183bcadadc1dceb23a4389d32804f8
mohammed-42 <mohammedmustafa5156@gmail.com>
2026-09-18 19:39:49 +0530

    Time: 28 ms (17.99%), Space: 58 MB (41.76%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -3,4 +3,12 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function(s, t) { let map = new Map(); for (let lett of s) { if (map.has(lett)) { map.set(lett, map.get(lett) + 1); } else { map.set(lett, 1); } } for (let sett of t) { if (map.has(sett) && map.get(sett) > 0) { map.set(sett, map.get(sett) - 1); } else { return false; } } for (let [key, value] of map) { if (value > 0) { return false; } } return true; };
+var isAnagram = function(s, t) {
+    let re=s.split("").sort().join("");
+    let res=t.split("").sort().join("");
+    if(re===res){
+        return true;
+    }else{
+        return false;
+    }
+};
